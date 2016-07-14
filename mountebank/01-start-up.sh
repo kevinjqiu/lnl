1601b7b6097cbc86b2b93c0cc51050159586d647
Kevin Jing Qiu <kevin@idempotent.ca>
2016-07-14 13:04:25 -0400

    Update

--- mountebank/01-start-up.sh
+++ mountebank/01-start-up.sh
@@ -1,7 +1,7 @@
 source fun.sh
 
-docker stop mountebank 2>/dev/null || true
-docker rm mountebank 2>/dev/null || true
+#docker stop mountebank 2>/dev/null || true
+#docker rm mountebank 2>/dev/null || true
 docker pull kevinjqiu/mountebank
 docker run --name mountebank -d kevinjqiu/mountebank
 docker inspect mountebank | grep IPAdd
