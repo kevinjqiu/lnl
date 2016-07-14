6b31f2f18e087d03f5a8168334a32b076575087f
Kevin Jing Qiu <kevin@idempotent.ca>
2016-07-14 11:43:28 -0400

    Update

--- mountebank/01-start-up.sh
+++ mountebank/01-start-up.sh
@@ -1,5 +1,6 @@
 source fun.sh
 
+docker stop mountebank 2>/dev/null || true
 docker rm mountebank 2>/dev/null || true
 docker pull kevinjqiu/mountebank
 docker run --name mountebank -d kevinjqiu/mountebank
